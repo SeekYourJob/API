4525060b56c269e58de32523b425f0437fa9eed4
Valentin Polo <me@valentinpolo.fr>
2015-07-28 16:50:13 +0200

    JWT dependency installed

--- config/app.php
+++ config/app.php
@@ -148,6 +148,7 @@ return [
          * From Composer...
          */
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
 
     ],
 
@@ -196,6 +197,9 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+
+        'JWTAuth'   => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
 
     ],
 
