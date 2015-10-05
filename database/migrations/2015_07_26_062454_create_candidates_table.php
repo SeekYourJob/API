d9ddea92bd50efe0852976a082d806a15f9c83a2
Valentin Polo <me@valentinpolo.fr>
2015-10-05 13:25:23 +0200

    Adding fields to Candidate

--- database/migrations/2015_07_26_062454_create_candidates_table.php
+++ database/migrations/2015_07_26_062454_create_candidates_table.php
@@ -16,6 +16,7 @@ class CreateCandidatesTable extends Migration
             $table->increments('id');
 
             $table->enum('grade', ['L1', 'L2', 'L3', 'M1', 'M2'])->default('M2');
+            $table->string('education');
             $table->boolean('printing_option')->default(false);
 
             $table->timestamps();
