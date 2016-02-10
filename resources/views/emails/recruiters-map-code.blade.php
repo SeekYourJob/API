4c03fd377dc9213026d043fd9f7c40178f3bef81
Valentin Polo <me@valentinpolo.fr>
2016-02-10 13:40:18 +0100

    Editing recruiter parking email

--- resources/views/emails/recruiters-map-code.blade.php
+++ resources/views/emails/recruiters-map-code.blade.php
@@ -3,9 +3,11 @@
 @section('content')
     Le Job Forum de la FGES approche à grand pas !<br><br>
 
-    Ci-dessous, vous trouverez le plan d'accès au parking auquel vous pourrez accéder le jour de l'évènement. L'entrée du parking se situe au <a href="https://goo.gl/maps/hHe52kshLS92"><strong>5 rue Roland</strong></a> tandis que l'entrée de la Faculté au <a href="https://goo.gl/maps/HDdnBmcEesJ2"><strong>41 rue du Port</strong></a> à Lille.<br><br>
+    Ci-dessous, le plan d'accès au parking accessible le jour de l'évènement. Celui-ci étant en rénovation le nombre de places est limité. Aussi, nous vous invitons à minimiser le nombre de véhicules dans la mesure du possible.<br><br>
 
-    Un code d'accès vous sera demandé, le voici : <strong>XXXX</strong>.<br><br>
+    L'entrée du parking se situe au <a href="https://goo.gl/maps/hHe52kshLS92"><strong>5 rue Roland</strong></a> tandis que l'entrée de la Faculté au <a href="https://goo.gl/maps/HDdnBmcEesJ2"><strong>41 rue du Port</strong></a> à Lille.<br><br>
+
+    <strong>Aucun code n'est nécessaire</strong>, il vous suffira de vous présenter en tant que participant au Job Forum de la FGES à l'entrée pour accéder au parking.<br><br>
 
     <img src="{{ $message->embed(public_path('img/gmaps.jpg')) }}" /><br><br>
 
